1603bdb6140ede29fd21584eb6f907086c8c1899
Pacharadanai1 <152575128+Pacharadanai1@users.noreply.github.com>
2025-03-12 14:52:03 +0700

    commit last work

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 8000
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send(`
-        <!doctype html>
-        <html>
-            <head>
-                <title>Express.js</title>
-            </head>
-            <body>
-                <h3>Welcome to Express.js</h3>
-                <b>Express.js Fast, unopinionated, minimalist <br/> web framework for Node.js</b>
-            </body>
-        </html>
-        `)
-        })
-        app.listen(port, () => {console.log('Server listening on port ' + port)})
+app.post('/api/form-post',(req,res) => {
+    let name = req.body.name || ''
+    let email = req.body.email || ''
+    let msg = req.body.message || ''
+
+    let text = `
+    <table border="1">
+    <caption>ข้อมูลทั่วไป</caption>
+    <tr><td>ชื่อ:</td> <td>${name}</td></tr>
+    <tr><td>อีเมล:</td> <td>${email}</td></tr>
+    <tr><td>ข้อความ:</td> <td>${msg}</td></tr>
+    </table>
+    `
+
+    res.send(text)
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port' + port)
+})
+   
